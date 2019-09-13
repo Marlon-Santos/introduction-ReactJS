6121754501f9475935774c5b50131d0cc18441d9
git <marlonergondossantos@gmail.com>
2019-09-13 12:17:55 -0300

    setState

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 
 class List extends Component {
   state = {
+    newTech: "",
     techs: ["node.js", "reactJs", "react native"]
+  };
+  onChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
   };
   render() {
     return (
-      <ul>
-        <li>node.js</li>
-        <li>reactJs</li>
-        <li>react Native</li>
-      </ul>
+      <form onSubmit={this.onSubmit}>
+        <ul>
+          {this.state.techs.map(value => {
+            return <li key={value}>{value}</li>;
+          })}
+          <input
+            type="text"
+            id="inputTech"
+            onChange={this.onChange}
+            value={this.state.newTech}
+          ></input>
+          <input type="submit"></input>
+        </ul>
+      </form>
     );
   }
 }
